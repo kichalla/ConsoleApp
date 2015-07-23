bda978e74d91880f591df930c1de5bc8666c5b30
Kiran Challa <kichalla@microsoft.com>
2015-07-23 09:26:14 -0700

    added runtime

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Framework.Runtime;
 
 namespace ConsoleApp1
 {
     public class Program
     {
+        private readonly IApplicationEnvironment _appEnv;
+
+        public Program(IApplicationEnvironment appEnv)
+        {
+            _appEnv = appEnv;
+        }
+
         public void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!. Runtime:{0}", _appEnv.RuntimeFramework.ToString());
         }
     }
 }
